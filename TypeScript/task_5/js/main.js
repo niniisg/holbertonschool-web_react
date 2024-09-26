8655b7735bb4ee2ca5d565d0d392d031a6c4dcad
niniisg <nisselsanchez@gmail.com>
2024-09-26 14:35:20 -0400

    adding task5

---
+++ TypeScript/task_5/js/main.js
@@ -0,0 +1,12 @@
+function SumMajorCredits(subject1, subject2) {
+    return {
+        credits: subject1.credits + subject2.credits,
+        brand: "MajorCredits"
+    };
+}
+function sumMinorCredits(subject1, subject2) {
+    return {
+        credits: subject1.credits + subject2.credits,
+        brand: "MinorCredits"
+    };
+}
