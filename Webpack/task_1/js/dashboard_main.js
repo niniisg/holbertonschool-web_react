80f26f47b9dcecf46592b6c8c8c4015696cbbf05
niniisg <nisselsanchez@gmail.com>
2025-04-19 19:18:16 -0400

    task1

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+
+$(document).ready(function () {
+    let count = 0;
+    function updateCounter() {
+        count += 1;
+  $('#count').text(`${count} clicks on the button`);
+
+  $('#click-me').on('click', _.debounce(updateCounter, 500));
+}
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append("<p id='count'></p>");
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
