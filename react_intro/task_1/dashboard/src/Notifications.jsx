287aeb215c7a1efdb31ed380959c6dc33f216f3d
niniisg <nisselsanchez@gmail.com>
2025-05-04 15:54:53 -0400

    addint task1

---
+++ react_intro/task_1/dashboard/src/Notifications.jsx
@@ -0,0 +1,9 @@
+function Notifications() {
+    return (
+      <div className="notifications">
+        <p>Here is the list of notifications</p>
+      </div>
+    );
+  }
+  
+  export default Notifications;
